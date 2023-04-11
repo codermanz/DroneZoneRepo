92cf1464c64d6ea95e5497b520f04c585a51e5b1
Saad Jeelani <saad.jeelani88@gmail.com>
2023-04-11 15:34:23 -0700

    Connected java client to janusgraph server, script ready for presentation. Updated Operator model

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,46 @@
+/**
+ * Sample code for connecting to gremlin server. Is used in tandem with air routes dataset from the book.
+ * Please ignore.
+ */
+
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+
+import java.util.*;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.neq;
+
+import org.apache.tinkerpop.gremlin.driver.ser.GraphBinaryMessageSerializerV1;
+import org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0;
+import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
+import org.apache.tinkerpop.gremlin.process.traversal.TraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import static org.apache.tinkerpop.gremlin.structure.T.*;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+import org.janusgraph.core.JanusGraph;
+import org.janusgraph.core.JanusGraphFactory;
+import org.javatuples.Tuple;
+
 class Test{
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        GraphTraversalSource g = traversal().withRemote(DriverRemoteConnection.using("localhost",8182,"g"));
+//        GraphTraversalSource g = traversal().withRemote(DriverRemoteConnection.using("conf/remote-graph.properties"));
+
+        List <Map<Object,Object>> vmaps = g.V().valueMap().toList();
+
+        System.out.println("\n\nThe following airports were found " + vmaps.size() + "\n");
+        for (Map <Object,Object> m : vmaps)
+        {
+            ArrayList code = (ArrayList) m.get("code");
+            ArrayList desc = (ArrayList) m.get("desc");
+            System.out.println(m);
+        }
+        System.out.println("Done, exiting");
+        g.close();
+
     }
 }
