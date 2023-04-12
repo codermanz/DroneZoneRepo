f12b939f719bc7360d34a19efcf414355c688a7b
Philip Weinmann <philipweinmann247+github@gmail.com>
2023-04-12 12:55:56 +0200

    removed python code and cleaning branch

--- classes/Agent.py
+++
@@ -1,6 +0,0 @@
-class Agent:
-    def __init__(self, identifier, health, location, active):
-        self.identifier = identifier
-        self.health = health
-        self.location = location
-        self.active = active
